6e86afe785303bc7eee1a4ed3a1ee0fafaadbc49
ninzzd <ninaad.desai1212@gmail.com>
2025-10-26 00:57:42 +0530

    Plotted the message signal Started defining the modulate function in fm_mod

--- fm_mod.py
+++ fm_mod.py
@@ -1,1 +1,11 @@
 import numpy as np
+class fm_mod:
+    def __init__(self,kf:float,wc:float,Ac:float):
+        self.kf = kf
+        self.wc = wc
+        self.Ac = Ac
+    def modulate(self,m:np.array,t:np.array):
+        del_t:float
+        if len(t) > 1:
+            del_t = t[1] - t[0] # uniform time samples (obviously)
+        psi = np.zeros()
--- sys.py
+++ sys.py
@@ -1,4 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
+'''
+    Ac = 1
+    Am = 1
+    kv = 75
+    fm = 10e3
+    fc = 10e3
 
-n:int = 512
-fs:float = 10e4
+'''
+# Sampling parameters
+n:int = 4096
+fs:float = 1e6
+Ts:float = 1/fs
+t = np.arange(0,n*Ts,Ts)
+
+# Message signal parameters
+Ac = 1
+m = Ac*np.cos(2*np.pi*1e3*t)
+
+# Message signal plot
+fig, ax = plt.subplots()
+ax.plot(t,m)
+ax.set(
+    xlabel="Time (s)",
+    ylabel="m(t)",
+    title="Message Signal Waveform"
+)
+ax.grid()
+plt.show()
