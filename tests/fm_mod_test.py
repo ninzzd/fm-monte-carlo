3f6a078ffbf7e4085fa63b188071c8a22c304dbe
ninzzd <ninaad.desai1212@gmail.com>
2025-10-26 02:36:52 +0530

    Restructured the entire repo Proper python project file structure

--- tests/fm_mod_test.py
+++ tests/fm_mod_test.py
@@ -1,48 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from fm_mod import fm_mod
+from fm.fm_mod import fm_mod
 '''
     Ac = 1
     Am = 1
     kv = 75
     fm = 10e3
     fc = 10e3
-
 '''
 # Sampling parameters
-n:int = 2**16
-fs:float = 1e7
+n:int = 2**12
+fs:float = 1e5
 Ts:float = 1/fs
 t = np.arange(0,n*Ts,Ts)
 
 # Message signal parameters
-Am = 10
-m = Am*np.cos(2*np.pi*1e3*t)
+Am = 1
+fm = 10
+m = Am*np.cos(2*np.pi*fm*t)
 
 # Modulator parameters
-kf = 75
+kf = 2e3
 Ac = 1
 fc = 1e3
 
 # Message signal plot
-fig, ax = plt.subplots(nrows=2,ncols=1)
-ax[0].plot(t,m)
-ax[0].set(
+fig, ax = plt.subplots()
+
+modulator:fm_mod = fm_mod(kf=kf,Ac=Ac,wc=2*np.pi*fc)
+psi = modulator.modulate(m,t,Ts)
+
+ax.plot(t,m,'r')
+ax.plot(t,psi,'b')
+ax.set(
     xlabel="Time (s)",
     ylabel="m(t)",
     title="Message Signal Waveform"
 )
-
-modulator:fm_mod = fm_mod(kf=kf,Ac=Ac,wc=2*np.pi*fc)
-psi = modulator.modulate(m,t,Ts)
-# print(psi)
-# print(m)
-ax[1].plot(t,psi)
-ax[1].set(
-    xlabel="Time (s)",
-    ylabel="phi(t)",
-    title="FM-Modulated Signal Waveform"
-)
+ax.grid()
 plt.show()
+fig.savefig("docs/fm_mod_test.png")
 
 
